3cdaca011452fdbcc5798abdaf8080ee20ff4161
Hossein <gholamianhossein96@gmail.com>
2022-06-04 11:12:48 +0430

    Update app.php

--- config/app.php
+++ config/app.php
@@ -4,6 +4,6 @@ return [
     'APP_TITLE' => 'ربات تلگرام',
     'BASE_URL'  => 'http://localhost:8000',
     'BASE_DIR'  =>  dirname(__DIR__),
-    'TOKEN'     => '5339480129:AAG0ANW5j2BoZmxAXdzu4K2zBXUOP5fHBtM',
+    'TOKEN'     => '',
     'API_URL'   =>  'https://api.telegram.org/bot'
 ];
